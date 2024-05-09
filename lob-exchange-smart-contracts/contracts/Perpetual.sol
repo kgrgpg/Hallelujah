2994ee83ad8363e04bc9be8a5add2f8917148c41
Keshav Gupta <keshav.pg@gmail.com>
2024-05-09 22:17:28 +0200

    Use PriceOracle for collateral value calc. Supported Assets. Funding rate.

--- lob-exchange-smart-contracts/contracts/Perpetual.sol
+++ lob-exchange-smart-contracts/contracts/Perpetual.sol
@@ -12,32 +12,54 @@ contract Perpetual {
         bool isLong;
         uint256 entryPrice;
         uint256 liquidationPrice;
+        uint256 fundingRate; // Funding rate to be adjusted over time
     }
 
     mapping(address => Position) public positions;
+    uint256 public longPositions;
+    uint256 public shortPositions;
 
     event PositionOpened(address indexed user, bool isLong, uint256 amount, uint256 entryPrice);
     event PositionClosed(address indexed user, bool isLong, uint256 amount, uint256 exitPrice);
+    event FundingRateAdjusted(address indexed user, uint256 fundingRate);
 
     constructor(address _collateralManagerAddress) {
         collateralManager = CollateralManager(_collateralManagerAddress);
     }
 
     function openPosition(bool isLong, uint256 amount, uint256 entryPrice) public {
-        require(collateralManager.getCollateralBalance(msg.sender, "ETH") >= amount, "Insufficient collateral");
+        require(collateralManager.getUSDValue(msg.sender) >= amount, "Insufficient collateral");
+
+        uint256 fundingRate = calculateFundingRate(isLong);
+
         positions[msg.sender] = Position({
             user: msg.sender,
             amount: amount,
             isLong: isLong,
             entryPrice: entryPrice,
-            liquidationPrice: isLong ? entryPrice * 8 / 10 : entryPrice * 12 / 10
+            liquidationPrice: isLong ? entryPrice * 8 / 10 : entryPrice * 12 / 10,
+            fundingRate: fundingRate
         });
+
+        if (isLong) {
+            longPositions += amount;
+        } else {
+            shortPositions += amount;
+        }
+
         emit PositionOpened(msg.sender, isLong, amount, entryPrice);
+        emit FundingRateAdjusted(msg.sender, fundingRate);
     }
 
     function closePosition(uint256 exitPrice) public {
         Position storage position = positions[msg.sender];
         require(position.amount > 0, "No open position");
+
+        if (position.isLong) {
+            longPositions -= position.amount;
+        } else {
+            shortPositions -= position.amount;
+        }
 
         emit PositionClosed(msg.sender, position.isLong, position.amount, exitPrice);
         delete positions[msg.sender];
@@ -45,5 +67,10 @@ contract Perpetual {
 
     function getPosition(address user) public view returns (Position memory) {
         return positions[user];
+    }
+
+    function calculateFundingRate(bool isLong) public view returns (uint256) {
+        uint256 imbalance = isLong ? longPositions - shortPositions : shortPositions - longPositions;
+        return imbalance / 100; // Example: 1% funding rate
     }
 }
