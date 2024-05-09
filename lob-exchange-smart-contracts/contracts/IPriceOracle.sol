f1727006f6f232a3100c3a4b73c2052e6d6b6891
Keshav Gupta <keshav.pg@gmail.com>
2024-05-10 01:34:08 +0200

    Removed hardhat project

--- lob-exchange-smart-contracts/contracts/IPriceOracle.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
-
-interface IPriceOracle {
-    function getPrice(string memory asset) external view returns (uint256);
-}
--- lob-exchange-smart-contracts/scripts/generate-key.ts
+++
@@ -1,5 +0,0 @@
-import { Wallet } from "ethers";
-
-const wallet = Wallet.createRandom();
-console.log("Private Key:", wallet.privateKey);
-console.log("Address:", wallet.address);
