2a8a63011b40fd34551cc9591c1b202c44ec58b4
Keshav Gupta <keshav.pg@gmail.com>
2024-05-14 21:21:46 +0200

    import correction

--- web/src/app/pages/index.tsx
+++ web/src/app/pages/index.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { fetchOrders } from '';
+import { fetchOrders } from '../../services/api';
 import { Subscription } from 'rxjs';
 
 const Home = () => {
